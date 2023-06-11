a60a0b7f79e6f37f96f2749f1ee069cfa73fbe94
Tullio Forneris <tullio@192.168.1.9>
2023-06-11 20:58:05 +0200

    work on property section

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,27 +1,218 @@
 import { Add } from '@mui/icons-material';
-import { Box, Stack, Typography } from '@mui/material';
-import { useList } from '@refinedev/core';
+import {Box, MenuItem, Select, Stack, TextField, Typography} from '@mui/material';
+import {useTable} from '@refinedev/core';
 import { useNavigate } from "react-router-dom";
 
 import { PropertyCard, CustomButton } from '../components';
+import {useMemo} from "react";
 
 export const AllProperties: React.FC = () => {
     const navigate = useNavigate();
+    const {
+        tableQueryResult: { data, isLoading, isError },
+        current, setCurrent,
+        setPageSize,
+        pageCount,
+        sorter, setSorter,
+        filters, setFilters,
+    } = useTable();
+
+    const allProperties = data?.data ?? [];
+
+    const currentPrice = sorter.find((item) => item.field === "price")?.order;
+
+    const toggleSort = (field: string) => {
+        setSorter([{ field, order: currentPrice === "asc" ? "desc" : "asc" }]);
+    };
+
+    const currentFilterValues = useMemo(() => {
+        const logicalFilters = filters.flatMap((item) =>
+            "field" in item ? item : [],
+        );
+
+        return {
+            title:
+                logicalFilters.find((item) => item.field === "title")?.value ||
+                "",
+            propertyType:
+                logicalFilters.find((item) => item.field === "propertyType")
+                    ?.value || "",
+        };
+    }, [filters]);
+
+    if (isLoading) return <Typography>Loading...</Typography>;
+    if (isError) return <Typography>Error!</Typography>;
 
     return (
         <Box>
-            <Stack
-                direction="row"
-                justifyContent="space-between"
-                alignItems="center">
-                <Typography fontSize={25} fontWeight={700}>
-                    All Properties
-                </Typography>
-                <CustomButton
-                    title={"Add Property"}
-                    handleClick={() => navigate("/properties/create")}
-                    icon={<Add />} />
-            </Stack>
+
+            {/*header*/}
+            <Box mt={2} sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 3
+            }}>
+                    <Stack
+                        width="100%"
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        mb={2}
+                    >
+                        <Typography fontSize={25} fontWeight={700}>
+                            {!allProperties.length ? "There are no Properties" : "All Properties"}
+                        </Typography>
+                        <CustomButton
+                            title={"Add Property"}
+                            handleClick={() => navigate("/properties/create")}
+                            icon={<Add />} />
+                    </Stack>
+
+            </Box>
+
+            {/*body*/}
+            <Box sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                gap: 3,
+                padding: 2,
+                borderRadius: 3,
+                backgroundColor: 'background.paper'
+            }}>
+                <Box
+                    display="flex"
+                    width="84%"
+                    justifyContent="space-between"
+                    flexWrap="wrap"
+                >
+                    <Box
+                        display="flex"
+                        gap={2}
+                        flexWrap="wrap"
+                        mb={{ xs: "20px", sm: 0 }}
+                    >
+                        <CustomButton
+                            title={`Sort price ${
+                                currentPrice === "asc" ? "↑" : "↓"
+                            }`}
+                            handleClick={() => toggleSort("price")}
+                        />
+                        <TextField
+                            variant="outlined"
+                            color="info"
+                            placeholder="Search by title"
+                            value={currentFilterValues.title}
+                            onChange={(e) => {
+                                setFilters([
+                                    {
+                                        field: "title",
+                                        operator: "contains",
+                                        value: e.currentTarget.value
+                                            ? e.currentTarget.value
+                                            : undefined,
+                                    },
+                                ]);
+                            }}
+                        />
+                        <Select
+                            variant="outlined"
+                            color="info"
+                            displayEmpty
+                            required
+                            inputProps={{ "aria-label": "Without label" }}
+                            defaultValue=""
+                            value={currentFilterValues.propertyType}
+                            onChange={(e) => {
+                                setFilters(
+                                    [
+                                        {
+                                            field: "propertyType",
+                                            operator: "eq",
+                                            value: e.target.value,
+                                        },
+                                    ],
+                                    "replace",
+                                );
+                            }}
+                        >
+                            <MenuItem value="">All</MenuItem>
+                            {[
+                                "Apartment",
+                                "Villa",
+                                "Farmhouse",
+                                "Condos",
+                                "Townhouse",
+                                "Duplex",
+                                "Studio",
+                                "Chalet",
+                            ].map((type) => (
+                                <MenuItem
+                                    key={type}
+                                    value={type.toLowerCase()}
+                                >
+                                    {type}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </Box>
+                </Box>
+
+                {allProperties.map((property: any) => (
+                    <PropertyCard key={property._id}
+                                  id={property._id}
+                                  title={property.title}
+                                  price={property.price}
+                                  location={property.location}
+                                  photo={property.photo}
+                    />
+                ))}
+
+                {/*footer*/}
+                {allProperties.length > 0 && (
+                    <Box display="flex" justifyContent="flex-end" width="100%" gap={2} flexWrap="wrap">
+                        <CustomButton
+                            title="Previous"
+                            handleClick={() => setCurrent((prev) => prev - 1)}
+                            disabled={!(current > 1)}
+                        />
+                        <Box
+                            display={{ xs: "hidden", sm: "flex" }}
+                            alignItems="center"
+                            gap="5px"
+                        >
+                            Page{" "}
+                            <strong>
+                                {current} of {pageCount}
+                            </strong>
+                        </Box>
+                        <CustomButton
+                            title="Next"
+                            handleClick={() => setCurrent((prev) => prev + 1)}
+                            disabled={current === pageCount}
+                        />
+                        <Select
+                            variant="outlined"
+                            color="info"
+                            displayEmpty
+                            required
+                            inputProps={{ "aria-label": "Without label" }}
+                            defaultValue={10}
+                            onChange={(e) =>
+                                setPageSize(
+                                    e.target.value ? Number(e.target.value) : 10,
+                                )
+                            }
+                        >
+                            {[10, 20, 30, 40, 50].map((size) => (
+                                <MenuItem key={size} value={size}>
+                                    Show {size}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </Box>
+                )}
+            </Box>
+
         </Box>
     );
 }
--- src/pages/property-details.tsx
+++ src/pages/property-details.tsx
@@ -1,7 +1,142 @@
+import {useDelete, useGetIdentity, useShow} from "@refinedev/core";
+import {useNavigate, useParams} from "react-router-dom";
+import {Box, Stack, Typography} from "@mui/material";
+import {ChevronLeft, MoreHoriz} from "@mui/icons-material";
+import {CustomButton} from "../components";
+
 export const PropertyDetails: React.FC = () => {
+    const navigate = useNavigate();
+    const { data: user } = useGetIdentity({
+        v3LegacyAuthProviderCompatible: true,
+    });
+    const { queryResult } = useShow();
+    const { mutate } = useDelete();
+    const { id } = useParams();
+
+    const { data, isLoading, isError } = queryResult;
+
+    const propertyDetails = data?.data ?? {};
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return <div>Something went wrong!</div>;
+    }
+
+    const isCurrentUser = user.email === propertyDetails.creator.email;
+
     return (
-        <div>
-            PropertyDetails
-        </div>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: 2,
+            borderRadius: 3,
+            backgroundColor: 'background.paper',
+        }}>
+
+            {/*header*/}
+            <Stack direction="row" gap={2} mb={2}>
+                <ChevronLeft sx={{
+                    borderRadius: '50%',
+                    cursor: 'pointer',
+                    '&:hover': {
+                        boxShadow: '0 20px 45px 0 rgba(176, 176, 176, 0.3)'
+                    }}} onClick={() => navigate(-1)}/>
+                <Typography>Details</Typography>
+            </Stack>
+
+            {/*body*/}
+            <Stack direction="row" gap={2}>
+
+                {/*main*/}
+                <Box sx={{
+                    display: 'flex',
+                    gap: 2,
+                }}>
+                    {/*first column*/}
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                    }}>
+                        <img style={{
+                            width: 541,
+                            height: 346,
+                            borderRadius: 1,
+                        }} src={propertyDetails.photo} alt="property"/>
+                    </Box>
+
+                    {/*second column*/}
+                    <Box sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 2,
+                    }}>
+                        {/*first img*/}
+                        <img style={{
+                            width: 203,
+                            height: 162,
+                            borderRadius: 1,
+                        }} src={propertyDetails.photo} alt="property"/>
+
+                        {/*clickable img*/}
+                        <Box sx={{position: 'relative'}}>
+
+                            <Box sx={{
+                                display: 'flex',
+                                textAlign: 'center',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                position: 'absolute',
+                                width: 203,
+                                height: 162,
+                                left: 0,
+                                top: 0,
+                                borderRadius: 1,
+                                color: 'white',
+                                background: 'rgba(0, 0, 0, 0.6)',
+                            }}>
+                                <Stack>+10</Stack>
+                            </Box>
+                            <img style={{
+                                width: 203,
+                                height: 162,
+                                borderRadius: 1,
+                            }} src={propertyDetails.photo} alt="property"/>
+                        </Box>
+                    </Box>
+                </Box>
+
+                {/*side*/}
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    gap: 2,
+                    border: '1px solid #E0E0E0',
+                    borderRadius: 1,
+                    padding: 2,
+                }}>
+                    <Stack width="100%" alignItems="end" justifyContent="flex-end">
+                        <MoreHoriz />
+                    </Stack>
+                    <img src={propertyDetails.photo} style={{
+                        width: 90,
+                        height: 90,
+                        borderRadius: '50%',
+                    }} />
+                    <Typography>Name</Typography>
+                    <Typography variant="caption" color="text.secondary">Role</Typography>
+                    <Typography variant="caption" color="text.secondary">Place</Typography>
+                    <Typography variant="subtitle2">Props</Typography>
+                    <Stack direction="row" justifyContent="center" gap={2}>
+                        <CustomButton title={'Message'} handleClick={() => {}} />
+                        <CustomButton title={'Call'} handleClick={() => {}} />
+                    </Stack>
+                </Box>
+            </Stack>
+        </Box>
     );
 }
